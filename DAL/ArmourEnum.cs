a8e648029072b005c5884038d9582b96d8aa578c
Mark Vermulst <m.vermulst@student.avans.nl>
2020-10-09 13:06:32 +0200

    added armour model

---
+++ DAL/ArmourEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    public enum ArmourEnum
+    {
+        Head,
+        Necklace,
+        Hands,
+        Chest,
+        Feet, 
+        Ring
+    }
+}
---
+++ DAL/Models/Armour.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Models
+{
+    public class Armour
+    {
+        public int Strength { get; set; }
+        private int Agility { get; set; }
+        private int Intelligence { get; set; }
+        private ArmourEnum ArmourType { get; set; }
+    }
+}
