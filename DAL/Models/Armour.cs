fb40955f614e10cc1a8fec32d7e91046b2a844a8
Mark Vermulst <m.vermulst@student.avans.nl>
2020-10-09 13:08:50 +0200

    updated model

--- DAL/Models/Armour.cs
+++ DAL/Models/Armour.cs
@@ -6,9 +6,11 @@ namespace DAL.Models
 {
     public class Armour
     {
+        private string Name { get; set; }
         public int Strength { get; set; }
         private int Agility { get; set; }
         private int Intelligence { get; set; }
+        private int Price { get; set; }
         private ArmourEnum ArmourType { get; set; }
     }
 }
