102b819af1a47d50872b90be390223f03f13473c
Job <contact@chu.mk>
2020-10-14 15:22:10 +0200

    Partially implemented repository usage instead of direct dbContext access

--- NinjaManager/Controllers/ShopController.cs
+++ NinjaManager/Controllers/ShopController.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Data;
+using System.Linq;
+using DAL;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -9,45 +12,47 @@ namespace NinjaManager.Controllers
 {
     public class ShopController : Controller
     {
-        private readonly NinjaManagerContext _context;
+        private readonly NinjaArmourRepository<NinjaArmour> _ninjaArmourRepository;
+        private readonly RepositoryBase<Ninja> _ninjaRepository;
+        private readonly RepositoryBase<Armour> _armourRepository;
 
         public ShopController(NinjaManagerContext context)
         {
-            _context = context;
+            _ninjaArmourRepository = new NinjaArmourRepository<NinjaArmour>(context);
+            _ninjaRepository = new RepositoryBase<Ninja>(context);
+            _armourRepository = new RepositoryBase<Armour>(context);
         }
 
         // GET: Shop
-        public IActionResult Index()
-        {
-            var ninjaManagerContext = _context.NinjaArmour.Include(n => n.Armour).Include(n => n.Ninja);
-            return View(ninjaManagerContext.ToList());
-        }
+        public IActionResult Index() => View(_ninjaArmourRepository.Get());
 
         // GET: Shop/Details/5
         public IActionResult Details(int? id)
         {
+            throw new NoNullAllowedException();
+
             if (id == null)
             {
                 return NotFound();
             }
 
-            var ninjaArmour = _context.NinjaArmour
-                .Include(n => n.Armour)
-                .Include(n => n.Ninja)
-                .FirstOrDefault(m => m.NinjaId == id);
-            if (ninjaArmour == null)
-            {
-                return NotFound();
-            }
+            //var ninjaArmour = _context.NinjaArmour
+            //    .Include(n => n.Armour)
+            //    .Include(n => n.Ninja)
+            //    .FirstOrDefault(m => m.NinjaId == id);
+            //if (ninjaArmour == null)
+            //{
+            //    return NotFound();
+            //}
 
-            return View(ninjaArmour);
+            //return View(ninjaArmour);
         }
 
         // GET: Shop/Create
         public IActionResult Create()
         {
-            ViewData["ArmourId"] = new SelectList(_context.Armour, "Id", "Name");
-            ViewData["NinjaId"] = new SelectList(_context.Ninja, "Id", "Name");
+            ViewData["ArmourId"] = new SelectList(_armourRepository.Get(), "Id", "Name");
+            ViewData["NinjaId"] = new SelectList(_ninjaRepository.Get(), "Id", "Name");
             return View();
         }
 
@@ -60,31 +65,32 @@ namespace NinjaManager.Controllers
         {
             if (ModelState.IsValid)
             {
-                _context.Add(ninjaArmour); 
-                _context.SaveChanges();
+                _ninjaArmourRepository.Add(ninjaArmour);
+                _ninjaArmourRepository.Save();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["ArmourId"] = new SelectList(_context.Armour, "Id", "Name", ninjaArmour.ArmourId);
-            ViewData["NinjaId"] = new SelectList(_context.Ninja, "Id", "Name", ninjaArmour.NinjaId);
+            ViewData["ArmourId"] = new SelectList(_armourRepository.Get(), "Id", "Name", ninjaArmour.ArmourId);
+            ViewData["NinjaId"] = new SelectList(_ninjaRepository.Get(), "Id", "Name", ninjaArmour.NinjaId);
             return View(ninjaArmour);
         }
 
         // GET: Shop/Edit/5
         public IActionResult Edit(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            throw new NotImplementedException();
+            //if (id == null)
+            //{
+            //    return NotFound();
+            //}
 
-            var ninjaArmour = _context.NinjaArmour.Find(id);
-            if (ninjaArmour == null)
-            {
-                return NotFound();
-            }
-            ViewData["ArmourId"] = new SelectList(_context.Armour, "Id", "Name", ninjaArmour.ArmourId);
-            ViewData["NinjaId"] = new SelectList(_context.Ninja, "Id", "Name", ninjaArmour.NinjaId);
-            return View(ninjaArmour);
+            //var ninjaArmour = _context.NinjaArmour.Find(id);
+            //if (ninjaArmour == null)
+            //{
+            //    return NotFound();
+            //}
+            //ViewData["ArmourId"] = new SelectList(_context.Armour, "Id", "Name", ninjaArmour.ArmourId);
+            //ViewData["NinjaId"] = new SelectList(_context.Ninja, "Id", "Name", ninjaArmour.NinjaId);
+            //return View(ninjaArmour);
         }
 
         // POST: Shop/Edit/5
@@ -94,54 +100,58 @@ namespace NinjaManager.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, [Bind("NinjaId,ArmourId")] NinjaArmour ninjaArmour)
         {
-            if (id != ninjaArmour.NinjaId)
-            {
-                return NotFound();
-            }
+            throw new NotImplementedException();
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(ninjaArmour);
-                    _context.SaveChanges();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!NinjaArmourExists(ninjaArmour.NinjaId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ArmourId"] = new SelectList(_context.Armour, "Id", "Name", ninjaArmour.ArmourId);
-            ViewData["NinjaId"] = new SelectList(_context.Ninja, "Id", "Name", ninjaArmour.NinjaId);
-            return View(ninjaArmour);
+            //if (id != ninjaArmour.NinjaId)
+            //{
+            //    return NotFound();
+            //}
+
+            //if (ModelState.IsValid)
+            //{
+            //    try
+            //    {
+            //        _context.Update(ninjaArmour);
+            //        _context.SaveChanges();
+            //    }
+            //    catch (DbUpdateConcurrencyException)
+            //    {
+            //        if (!NinjaArmourExists(ninjaArmour.NinjaId))
+            //        {
+            //            return NotFound();
+            //        }
+            //        else
+            //        {
+            //            throw;
+            //        }
+            //    }
+            //    return RedirectToAction(nameof(Index));
+            //}
+            //ViewData["ArmourId"] = new SelectList(_context.Armour, "Id", "Name", ninjaArmour.ArmourId);
+            //ViewData["NinjaId"] = new SelectList(_context.Ninja, "Id", "Name", ninjaArmour.NinjaId);
+            //return View(ninjaArmour);
         }
 
         // GET: Shop/Delete/5
         public IActionResult Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            throw new NotImplementedException();
 
-            var ninjaArmour =  _context.NinjaArmour
-                .Include(n => n.Armour)
-                .Include(n => n.Ninja)
-                .FirstOrDefault(m => m.NinjaId == id);
-            if (ninjaArmour == null)
-            {
-                return NotFound();
-            }
+            //if (id == null)
+            //{
+            //    return NotFound();
+            //}
 
-            return View(ninjaArmour);
+            //var ninjaArmour =  _context.NinjaArmour
+            //    .Include(n => n.Armour)
+            //    .Include(n => n.Ninja)
+            //    .FirstOrDefault(m => m.NinjaId == id);
+            //if (ninjaArmour == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //return View(ninjaArmour);
         }
 
         // POST: Shop/Delete/5
@@ -149,15 +159,17 @@ namespace NinjaManager.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult DeleteConfirmed(int id)
         {
-            var ninjaArmour = _context.NinjaArmour.Find(id);
-            _context.NinjaArmour.Remove(ninjaArmour);
-            _context.SaveChanges();
-            return RedirectToAction(nameof(Index));
+            throw new NotImplementedException();
+
+            //var ninjaArmour = _context.NinjaArmour.Find(id);
+            //_context.NinjaArmour.Remove(ninjaArmour);
+            //_context.SaveChanges();
+            //return RedirectToAction(nameof(Index));
         }
 
-        private bool NinjaArmourExists(int id)
-        {
-            return _context.NinjaArmour.Any(e => e.NinjaId == id);
-        }
+        //private bool NinjaArmourExists(int id)
+        //{
+        //    return _context.NinjaArmour.Any(e => e.NinjaId == id);
+        //}
     }
 }
